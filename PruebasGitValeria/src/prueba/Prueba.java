6a46f12282c54754bd1b17dc883756eef42ae69a
Valeria Valencia <valeriayessenia.valenciaba@elorrieta-errekamari.com>
2022-03-07 15:48:55 +0100

    le meto la x

--- PruebasGitValeria/src/prueba/Prueba.java
+++ PruebasGitValeria/src/prueba/Prueba.java
@@ -1,10 +1,12 @@
 package prueba;
 
 public class Prueba {
+	
+	private static int x = 10;
 
 	public static void main(String[] args) {
 		System.out.println("Hola mundo");
-
+		System.out.println("sdas" + x);
 	}
 
 }
