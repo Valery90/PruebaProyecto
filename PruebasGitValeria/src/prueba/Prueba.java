f350eedfc2605c9d0e6d3047e16f4bfd9e8c6795
Valeria Valencia <valeriayessenia.valenciaba@elorrieta-errekamari.com>
2022-03-09 16:01:25 +0100

    Prueba proyecto

--- PruebasGitValeria/src/prueba/Prueba.java
+++ PruebasGitValeria/src/prueba/Prueba.java
@@ -1,12 +1,10 @@
 package prueba;
 
 public class Prueba {
-	
-	private static int x = 10;
 
 	public static void main(String[] args) {
 		System.out.println("Hola mundo");
-		System.out.println("sdas" + x);
+
 	}
 
 }
