953bca2a97b4b7ba3d88cc1007344adc18b93b53
Valeria Valencia <valeriayessenia.valenciaba@elorrieta-errekamari.com>
2022-03-09 16:22:27 +0100

    soy valeria cambio

--- PruebasGitValeria/src/prueba/Prueba.java
+++ PruebasGitValeria/src/prueba/Prueba.java
@@ -5,6 +5,7 @@ public class Prueba {
 	public static void main(String[] args) {
 		System.out.println("Hola mundo");
 		System.out.println("Hola mundo de Ana");
+		System.out.println("soy valeria");
 	}
 
 }
